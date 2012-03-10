f88439f2003863ee7a1002d02d8325ce6cd919df
narunetto <narunetto@gilheney-mafuyu.minchi.imcumm.in>
2012-03-10 11:14:59 -0800

    add really stupid things to the code.

--- vlcweb/logout.php
+++ vlcweb/logout.php
@@ -1,5 +1,6 @@
 <?php
-setcookie('user', '', time() - 42000);
-setcookie("sessionid",'', time() - 42000);
-header('Location: http://imcumm.in/vlcweb/login.php');
+// oh boy we're rearing shit
+setcookie('user', '', time() - 42000); // this is what it sounds like, when the cookies cry
+setcookie("sessionid",'', time() - 42000); // murder
+header('Location: http://imcumm.in/vlcweb/login.php'); // cheeze it, it's the cops.
 ?>
